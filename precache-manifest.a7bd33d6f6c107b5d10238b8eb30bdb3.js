04b6a9499bb4ffb105f97928d04d27e6632df8eb
thedann8@gmail.com <thedann8@gmail.com>
2020-02-14 10:10:56 +0100

    Updates

---
+++ precache-manifest.a7bd33d6f6c107b5d10238b8eb30bdb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c694068eb20a7251ffde788b3fbd57db",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "6402a54fe50e0b2e180f",
+    "url": "/snake/static/css/main.ca6cd6c3.chunk.css"
+  },
+  {
+    "revision": "7d4d3006bc9bda4725ba",
+    "url": "/snake/static/js/2.a4da1dec.chunk.js"
+  },
+  {
+    "revision": "6402a54fe50e0b2e180f",
+    "url": "/snake/static/js/main.59036f7f.chunk.js"
+  },
+  {
+    "revision": "ca6cfa26c4ae4fd890d9",
+    "url": "/snake/static/js/runtime-main.0386ce30.js"
+  }
+]);
